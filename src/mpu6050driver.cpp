ccdc6a05ef623c29fb7a69556ad039056932e6e1
josefgst <josefgstoettner@live.at>
2022-11-16 17:42:58 +0800

    change to rad/s

--- src/mpu6050driver.cpp
+++ src/mpu6050driver.cpp
@@ -47,9 +47,10 @@ void MPU6050Driver::handleInput()
   message.linear_acceleration.y = mpu6050_->getAccelerationY();
   message.linear_acceleration.z = mpu6050_->getAccelerationZ();
   message.angular_velocity_covariance[0] = {0};
-  message.angular_velocity.x = mpu6050_->getAngularVelocityX();
-  message.angular_velocity.y = mpu6050_->getAngularVelocityY();
-  message.angular_velocity.z = mpu6050_->getAngularVelocityZ();
+  float deg_to_rad = 0.0174533;
+  message.angular_velocity.x = mpu6050_->getAngularVelocityX()*deg_to_rad;
+  message.angular_velocity.y = mpu6050_->getAngularVelocityY()*deg_to_rad;
+  message.angular_velocity.z = mpu6050_->getAngularVelocityZ()*deg_to_rad;
   // Invalidate quaternion
   message.orientation_covariance[0] = -1;
   message.orientation.x = 0;
